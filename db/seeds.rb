939a03be7f9601a8b90abfd58d2fc32a8fb555e0
David Caballero <david.caballero@code.berlin>
2019-11-13 14:44:35 +0100

    WikiDavid completed. Front end default, will add own style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all
+
+def fake_content
+  paragraphs = []
+  paragraphs << Faker::TvShows::GameOfThrones.quote
+  paragraphs << "![](https://source.unsplash.com/random/1000x500)"
+  paragraphs << "## #{Faker::Quote.famous_last_words}"
+  paragraphs << Faker::TvShows::GameOfThrones.quote
+  paragraphs << "**#{Faker::JapaneseMedia::DragonBall.character}** #{Faker::ChuckNorris.fact}"
+  paragraphs << "## #{Faker::Quote.yoda}"
+  paragraphs << "#{Faker::Quote.matz} [#{Faker::Commerce.product_name}](#{Faker::Internet.url}) #{Faker::Quote.most_interesting_man_in_the_world}"
+  paragraphs << Faker::TvShows::TwinPeaks.quote
+  paragraphs.flatten.join("\n\n")
+end
+
+15.times do
+  Article.create(
+    title: Faker::GreekPhilosophers.quote,
+    content: fake_content
+  )
+end
